68c20b2b062272debc446e1bf09e9ab37f6c4910
khmalz <akmalrma1710@gmail.com>
2025-11-09 23:02:52 +0700

    add swagger on nestjs

--- apps/api-nest/src/main.ts
+++ apps/api-nest/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
+
+   const config = new DocumentBuilder().setTitle('Reminder Task Documentation').setDescription('The API description').setVersion('1.0').build();
+   const document = SwaggerModule.createDocument(app, config);
+   SwaggerModule.setup('api', app, document);
+
    await app.listen(process.env.PORT ?? 3000);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
