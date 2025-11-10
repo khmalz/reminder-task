486c4b6eb2434bbecd64617c0d46f7a1685ca24d
khmalz <akmalrma1710@gmail.com>
2025-11-10 14:17:49 +0700

    change from ts to js in next framework

--- apps/web/.eslintignore.mjs
+++ apps/web/.eslintignore.mjs
@@ -1,3 +1,3 @@
 import { globalIgnores } from "eslint/config";
 
-export default globalIgnores(["**/.*/**/*", "**/node_modules", "**/.next/**/*", "**/out/**/*", "**/.vscode/**/*", "**/dist/**/*", "**/build/**/*", "**/public/**/*", "next-env.d.ts"]);
+export default globalIgnores(["**/.*/**/*", "**/node_modules", "**/.next/**/*", "**/out/**/*", "**/.vscode/**/*", "**/dist/**/*", "**/build/**/*", "**/public/**/*"]);
--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,14 +1,12 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
 import reactHooks from 'eslint-plugin-react-hooks';
 import { defineConfig } from "eslint/config";
 import eslintignore from "./.eslintignore.mjs";
 
 const eslintConfig = defineConfig([
-   ...nextVitals,
-   ...nextTs,
-   eslintignore,
-   reactHooks.configs.flat.recommended,
+  ...nextVitals,
+  eslintignore,
+  reactHooks.configs.flat.recommended,
 ]);
 
 export default eslintConfig;
